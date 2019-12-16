e86c55a8a92c760f27cdf4d3e65332d472e1fa49
rohyer <guilhermerl.dev@gmail.com>
2019-12-16 19:28:26 -0300

    secao clientes

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,25 @@ $('#services ul').owlCarousel({
 	}
 });
 
+$('#clients ul').owlCarousel({
+	loop: true,
+	reponsiveClass: true,
+	responsive:{
+		0:{
+			items: 1,
+			nav: true
+		},
+		600:{
+			items: 1,
+			nav: true
+		},
+		1000:{
+			items: 1,
+			nav: true
+		}
+	}
+});
+
 // ================================================
 // ==================== HEADER ====================
 
--- elements/e-clients.php
+++ elements/e-clients.php
@@ -1,8 +1,38 @@
+<?php
+$args = array(
+    'category_name' => 'clients'
+);
+?>
+
 <section id="clients" class="section-site">
     <div class="container-custom">
         <div class="row">
-            <div class="col-12">
+            <!-- <div class="col-12">
                 <h4>Our clients feedback</h4>
+            </div> -->
+
+            <div class="col-12">
+                <ul class="owl-carousel owl-theme">
+                <?php
+                $query = new WP_Query( $args );
+                if ( $query->have_posts() ) {
+                    while ( $query->have_posts() ) {
+                        $query->the_post();
+                ?>
+                <li id="post-<?php the_ID(); ?>" class="clients-post">
+                    <div class="clients-img">
+                        <?php the_post_thumbnail(); ?>
+                    </div>
+                    <div class="clients-content">
+                        <span><?php the_title(); ?></span>
+                        <p><?php the_content() ?></p>
+                    </div>
+                </li>
+                <?php
+                    }
+                }
+                ?>
+                </ul>
             </div>
         </div>
     </div>
